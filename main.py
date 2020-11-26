ec06900deb1fefd3329349d66183057fe23748ce
Ristal <ristaltvgames@gmail.com>
2020-11-26 11:52:26 +0300

    начал писать функцию факторизации

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+def factorization(n):
+    p = []
+    d = 2
+    while d * d <= n:
+        while n % d == 0:
+            p.append(d)
+            n //= d
+        d += 1
+    if n > 1:
+        p.append(n)
+    ans = []
+    i = 0
+    
 a, b = int(input()), int(input())   # вводим числа
 if a == b == 1:                     # небольшая проверка на 1
     print(1)
-#else:
+else:
